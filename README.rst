f99faa8b421f3689b9630065e990b2b56ad3ea3d
Steve Schmerler <git@elcorto.com>
2017-05-03 01:36:31 +0200

    API: benchmarks: merge plot.py and plot.py, add benchmark docs
    Use tempfile for writing benchmark files. Detect existence of
    results.json and only plot.

--- README.rst
+++ README.rst
@@ -1,17 +1,35 @@
 findsame
 ========
 
-Find duplicate files and directories. As other tools such as ``fdupes`` or
-``fslint``'s ``fdindup`` (see ``/usr/share/fslint/fslint/findup``), we also use
-simple file hashes.
+Find duplicate files and directories.
 
-But additionally, we report duplicate directories as well, using a
-Merkle tree for directory hash calculation.
+As other tools we use file hashes but additionally, we report duplicate
+directories as well, using a Merkle tree for directory hash calculation.
 
-example
--------
+usage
+-----
 
-The default output format is json, where::
+::
+
+    $ ./findsame.py -h
+    usage: findsame.py [-h] [-v] [-n NCORES] [-b BLOCKSIZE]
+                       file/dir [file/dir ...]
+
+    Find same files and dirs based on file hashes.
+
+    positional arguments:
+      file/dir              files and/or dirs to compare
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -v, --verbose         verbose
+      -n NCORES, --ncores NCORES
+                            number of processes for parallel hash calc in Merkle
+                            tree
+      -b BLOCKSIZE, --blocksize BLOCKSIZE
+                            read-in blocksize (byte) in hash calculation [1048576]
+
+The output format is json::
 
     {
         hash1: {
@@ -144,23 +162,29 @@ Run ``nosetests3`` (maybe ``apt-get install python3-nose`` before (Debian)).
 
 benchmarks
 ----------
-You may run the benchmark suite to find the best blocksize and number of cores
+You may run the benchmark script to find the best blocksize and number of cores
 for hash calculations::
 
     $ cd benchmark
-    $ ./benchmark.py /path/to/tmpdir
-    $ ./plot.py /path/to/tmpdir
+    $ rm -rf files results.json; ./benchmark.py
 
-This writes test files of various size to ``/path/to/tmpdir``. Tune
-``collection_size`` in ``benchmark.py`` for more and bigger test files.
+This writes test files of various size to ``benchmark/files``. Tune
+``collection_size`` in ``benchmark.main()`` for more and bigger test files.
+A collection is a number of files whose total size is ``collection_size``.
+Default is 100 MiB, i.e. files from 1 x 100MiB to 800 x 128 kiB files.
 
-Bottom line:
+Bottom line (test system: Lenovo E330, Samsung 840 Evo SSD, Core i3-3120M)
 
 * blocksizes around 512 kiB (``--blocksize 524288``) work best for all file
   sizes, even though the variation to worst timings is at most factor 1.25
   (e.g. 1 vs. 1.25 seconds)
-* don't use multiple cores as this actually slows things down since the hashing
-  seems to be IO-bound (reading is slower than hashing blocks)
-* there is a strong dependence on file size (up to factor 2), may be related to
-  disk cache size (runtime keeps increasing until certain characteristic file
-  sizes and then drops) .. not fully investigated yet
+* using multiple cores actually slows things down since the hashing seems to be
+  IO-bound (reading is slower than hashing blocks)
+* there is a strong (up to factor 2) and non-monotonic dependence on file size,
+  may be related to disk cache size (runtime keeps increasing until certain
+  characteristic file sizes and then drops) .. not fully investigated yet
+
+other tools
+-----------
+* ``fdupes``
+* ``fdindup`` from ``fslint``
--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,10 +1,110 @@
 #!/usr/bin/python3
 
+"""
+basic data struct for param study:
+
+params = [{'foo': 1, 'bar': 'lala', ...},  # parameter set 1
+          {'foo': 2, 'bar': 'zzz', ...},   # parameter set 2
+          ...                              # ...
+         ]
+
+These are the basis of a pandas DataFrame (much like an SQL table, 2D array
+w/ named columns and in case of DataFrame also variable data types) with
+columns 'foo' and 'bar'. Benchmark results (column 'timing') are appended in
+each run.
+
+Below we have some helper functions which assist in creating `params`.
+Basically, we define the to-be-varied parameters as "named sequences" (i.e.
+list of dicts) and use itertools.product to loop over them.
+
+    >>> from findsame.benchmark import benchmark as bm
+    >>> import itertools
+    >>> x=bm.seq2dicts('a', [1,2,3])
+    >>> x
+    [{'x': 1}, {'x': 2}, {'x': 3}]
+    >>> y=bm.seq2dicts('y', ['xx','yy','zz'])
+    >>> y
+    [{'y': 'xx'}, {'y': 'yy'}, {'y': 'zz'}]
+    >>> list(itertools.product(x,y))
+    [({'x': 1}, {'y': 'xx'}),
+     ({'x': 1}, {'y': 'yy'}),
+     ({'x': 1}, {'y': 'zz'}),
+     ({'x': 2}, {'y': 'xx'}),
+     ({'x': 2}, {'y': 'yy'}),
+     ({'x': 2}, {'y': 'zz'}),
+     ({'x': 3}, {'y': 'xx'}),
+     ({'x': 3}, {'y': 'yy'}),
+     ({'x': 3}, {'y': 'zz'})]
+
+    >>> bm.loops2params(itertools.product(x,y))
+    [{'x': 1, 'y': 'xx'},
+     {'x': 1, 'y': 'yy'},
+     {'x': 1, 'y': 'zz'},
+     {'x': 2, 'y': 'xx'},
+     {'x': 2, 'y': 'yy'},
+     {'x': 2, 'y': 'zz'},
+     {'x': 3, 'y': 'xx'},
+     {'x': 3, 'y': 'yy'},
+     {'x': 3, 'y': 'zz'}]
+
+The logic of the param study is entirely contained in the creation of `params`.
+E.g., if parameters shall be varied together, simply use ``mkparams(zip(x,y),
+z)``. The nestings from zip() are flattened in loops2params().
+
+    >>> z=bm.seq2dicts('z', [None, 1.2, 'X'])
+    >>> z
+    [{'z': None}, {'z': 1.2}, {'z': 'X'}]
+    >>> list(itertools.product(zip(x,y),z))
+    [(({'x': 1}, {'y': 'xx'}), {'z': None}),
+     (({'x': 1}, {'y': 'xx'}), {'z': 1.2}),
+     (({'x': 1}, {'y': 'xx'}), {'z': 'X'}),
+     (({'x': 2}, {'y': 'yy'}), {'z': None}),
+     (({'x': 2}, {'y': 'yy'}), {'z': 1.2}),
+     (({'x': 2}, {'y': 'yy'}), {'z': 'X'}),
+     (({'x': 3}, {'y': 'zz'}), {'z': None}),
+     (({'x': 3}, {'y': 'zz'}), {'z': 1.2}),
+     (({'x': 3}, {'y': 'zz'}), {'z': 'X'})]
+
+    >>> bm.loops2params(itertools.product(zip(x,y),z))
+    [{'x': 1, 'y': 'xx', 'z': None},
+     {'x': 1, 'y': 'xx', 'z': 1.2},
+     {'x': 1, 'y': 'xx', 'z': 'X'},
+     {'x': 2, 'y': 'yy', 'z': None},
+     {'x': 2, 'y': 'yy', 'z': 1.2},
+     {'x': 2, 'y': 'yy', 'z': 'X'},
+     {'x': 3, 'y': 'zz', 'z': None},
+     {'x': 3, 'y': 'zz', 'z': 1.2},
+     {'x': 3, 'y': 'zz', 'z': 'X'}]
+
+If you want a parameter which is constant, use a length one list and put it in
+the loops:
+
+    >>> c=bm.seq2dicts('c', ['const'])
+    >>> bm.loops2params(itertools.product(zip(x,y),z,c))
+    [{'a': 1, 'c': 'const', 'y': 'xx', 'z': None},
+     {'a': 1, 'c': 'const', 'y': 'xx', 'z': 1.2},
+     {'a': 1, 'c': 'const', 'y': 'xx', 'z': 'X'},
+     {'a': 2, 'c': 'const', 'y': 'yy', 'z': None},
+     {'a': 2, 'c': 'const', 'y': 'yy', 'z': 1.2},
+     {'a': 2, 'c': 'const', 'y': 'yy', 'z': 'X'},
+     {'a': 3, 'c': 'const', 'y': 'zz', 'z': None},
+     {'a': 3, 'c': 'const', 'y': 'zz', 'z': 1.2},
+     {'a': 3, 'c': 'const', 'y': 'zz', 'z': 'X'}]
+
+Then we define a callback function `func`, which takes only one parameter dict
+``{'a': 1, 'b': 'xx'}``, runs the benchmark and returns a dict ``{'timing':
+1.234}``, which gets merged with the current parameter dict and appended to the
+DataFrame. `func` is called on all `params` in the `run` helper function.
+"""
+
 from io import IOBase
 from itertools import product
-import timeit, sys, os, copy
+import timeit, os, copy
+from tempfile import mkdtemp
+
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 pj = os.path.join
 
@@ -34,10 +134,10 @@ def is_seq(seq):
     if isinstance(seq, str) or \
        isinstance(seq, IOBase) or \
        isinstance(seq, dict):
-       return False
+        return False
     else:
         try:
-            x=iter(seq)
+            _ = iter(seq)
             return True
         except:
             return False
@@ -67,7 +167,11 @@ def params2df(params):
 
 
 def mkparams(*args):
-    return [merge_dicts(flatten(entry)) for entry in product(*args)]
+    return loops2params(product(*args))
+
+
+def loops2params(loops):
+    return [merge_dicts(flatten(entry)) for entry in loops]
 
 
 def run(df, func, params):
@@ -106,17 +210,17 @@ def bytes_linspace(start, stop, num):
                                  num).astype(int))
 
 
-def write(tmpdir, filesize_lst, collection_size):
+def write(testdir, filesize_lst, collection_size):
     assert filesize_lst.max() <= collection_size
     print("writing data")
     filesize_dr_lst = []
     for filesize in filesize_lst:
         filesize_str = size2str(filesize)
         print("filesize: {}".format(filesize_str))
-        dr = pj(tmpdir, 'filesize_{}'.format(filesize_str))
+        dr = pj(testdir, 'filesize_{}'.format(filesize_str))
         filesize_dr_lst.append(dr)
         if not os.path.exists(dr):
-            os.makedirs(dr)
+            os.makedirs(dr, exist_ok=True)
             nfiles = collection_size // filesize
             data = b'x'*filesize
             for idx in range(nfiles):
@@ -137,11 +241,37 @@ def func(dct, stmt=None, setup=None):
 
 
 def params_filter(params):
-    return [p for p in params if p['blocksize'] <= p['filesize']] 
+    return [p for p in params if p['blocksize'] <= p['filesize']]
 
 
-if __name__ == '__main__':
-    tmpdir = sys.argv[1]
+def plot(study, df, xprop, yprop, cprop, plot='plot'):
+    fig,ax = plt.subplots()
+    df = df[df['study'] == study]
+    df = df.sort_values(xprop)
+    xticks = []
+    xticklabels = []
+    for const in np.sort(df[cprop].unique()):
+        msk = df[cprop] == const
+        label = df[msk][cprop].values[0]
+        x = df[msk][xprop]
+        y = df[msk][yprop]
+        getattr(ax, plot)(x, y, 'o-', label=label)
+        if len(x) > len(xticks):
+            xticks = x
+            xprop_str = xprop + '_str'
+            sel = xprop_str if xprop_str in df[msk].columns else xprop
+            xticklabels = df[msk][sel]
+
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels, rotation=45)
+    ax.set_xlabel(xprop)
+    ax.set_ylabel(yprop)
+    fig.subplots_adjust(bottom=0.2)
+    ax.legend(title=cprop.replace('_str',''))
+
+
+def main(tmpdir):
+    os.makedirs(tmpdir, exist_ok=True)
 
     setup = "from findsame import findsame as fs"
     stmt = "fs.main({files_dirs}, ncores={ncores}, blocksize={blocksize})"
@@ -149,20 +279,22 @@ if __name__ == '__main__':
     df = pd.DataFrame()
     collection_size = 100*MiB
     params = []
-    
+
     # test individual file sizes
-    cases = [(bytes_linspace(512*kiB, collection_size, 3),
+    cases = [(bytes_linspace(128*kiB, collection_size, 4),
               bytes_logspace(10*kiB, collection_size, 10),
               'blocksize'),
-             (bytes_linspace(512*kiB, collection_size, 10),
+             (bytes_linspace(128*kiB, collection_size, 40),
               [64*kiB, 512*kiB],
-              'filesize')] 
+              'filesize')]
 
     for filesize, blocksize, study in cases:
-        filesize_dr = write(tmpdir, filesize, collection_size)
+        testdir = mkdtemp(dir=tmpdir)
+        filesize_dr = write(testdir, filesize, collection_size)
         this = mkparams(zip(seq2dicts('filesize', filesize),
                             seq2dicts('filesize_dr', filesize_dr),
-                            seq2dicts('filesize_str', list(map(size2str, filesize))),
+                            seq2dicts('filesize_str', list(map(size2str,
+                                                               filesize))),
                             seq2dicts('files_dirs', [[x] for x in filesize_dr])),
                         seq2dicts('study', [study]),
                         seq2dicts('ncores', [1]),
@@ -170,24 +302,39 @@ if __name__ == '__main__':
                             seq2dicts('blocksize_str', list(map(size2str,
                                                                 blocksize)))))
         params += this
-        
+
 ##    params = params_filter(params)
-    
+
     # collection of different file sizes
-    filesize = bytes_linspace(512*kiB, collection_size, 5)
+    filesize = bytes_linspace(128*kiB, collection_size, 5)
     blocksize = [512*kiB]
-    testdir = pj(tmpdir, 'collection')
+    testdir = mkdtemp(dir=tmpdir)
     filesize_dr = write(testdir, filesize, collection_size)
-    
+
     this = mkparams(seq2dicts('files_dirs', [[testdir]]),
-                    seq2dicts('study', ['collection']),
+                    seq2dicts('study', ['all_sizes']),
                     seq2dicts('ncores', [1,2,4]),
                     zip(seq2dicts('blocksize', blocksize),
                         seq2dicts('blocksize_str', list(map(size2str,
-                        blocksize)))))
+                                                            blocksize)))))
     params += this
-    
-    df = run(df, lambda p: func(p, stmt, setup), params)
-    print(df)
-    with open(pj(tmpdir, 'results.json'), 'w') as fd:
-        fd.write(df.to_json())
+
+    return run(df, lambda p: func(p, stmt, setup), params)
+
+
+if __name__ == '__main__':
+    tmpdir = './files'
+    results = './results.json'
+    if os.path.exists(results):
+        print("{} found, just plotting".format(results))
+        df = pd.io.json.read_json(results)
+    else:
+        print("running benchmark")
+        df = main(tmpdir)
+        df.to_json(results)
+
+    plot('blocksize', df, 'blocksize', 'timing', 'filesize_str', plot='semilogx')
+    plot('filesize', df, 'filesize', 'timing', 'blocksize_str')
+    plot('all_sizes', df, 'ncores', 'timing', 'blocksize_str')
+
+    plt.show()
